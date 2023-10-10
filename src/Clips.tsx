02964041c266e82f0d235d2a0f7b9ba850e26479
spacemonkeyJT <spacemonkeyjt1@gmail.com>
2023-10-10 17:24:23 +0800

    wip

--- src/Clips.tsx
+++ src/Clips.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { User, getUser } from './twitch';
+import { Clip, User, getAllClips, getUser } from './twitch';
 
 interface Props {
   username: string;
@@ -8,15 +8,58 @@ interface Props {
 
 export const Clips = ({ username, token }: Props) => {
   const [user, setUser] = useState<User | undefined>();
+  const [search, setSearch] = useState<string>('');
+  const [allClips, setAllClips] = useState<Clip[]>([]);
+  const [filteredClips, setFilteredClips] = useState<Clip[]>([]);
+  const [searchCommitted, setSearchCommitted] = useState<string>('');
+  const [loading, setLoading] = useState(true);
 
+  // Get the data
   useEffect(() => {
+    async function fetchData() {
+      const user = await getUser(username, token);
+      setUser(user);
+      setAllClips(await getAllClips(user.id, token));
+      setLoading(false);
+    }
     if (token && username) {
-      getUser(username, token).then(setUser);
+      fetchData();
     }
   }, [username, token]);
 
+  // Delay searching until the user finish entering the search term
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setSearchCommitted(search);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, [search]);
+
+  useEffect(() => {
+    if (allClips.length > 0 && searchCommitted) {
+      const clips: Clip[] = [];
+      const searchExpr = new RegExp(`\\b${searchCommitted}\\b`, 'i');
+      for (const clip of allClips) {
+        if (searchExpr.exec(clip.title)) {
+          clips.push(clip);
+        }
+      }
+      setFilteredClips(clips);
+    } else {
+      setFilteredClips([]);
+    }
+  }, [allClips, searchCommitted]);
+
   if (user) {
-    return <div>Clips for {user.display_name}</div>
+    return <div className="clips">
+      <img className="userlogo" src={user.profile_image_url} alt={user.display_name} />
+      <div className="userdesc">Finding clips for <b>{user.display_name}</b></div>
+      {loading && <div>Loading...</div>}
+      {!loading && <>
+        <input type="text" value={search} onChange={e => setSearch(e.target.value)} />
+        <pre>{JSON.stringify(filteredClips, null, 2)}</pre>
+      </>}
+    </div>
   } else {
     return null;
   }
--- src/twitch.ts
+++ src/twitch.ts
@@ -11,14 +11,55 @@ export interface User {
   offline_image_url: string;
 }
 
-export async function getUser(username: string, token: string) {
-  const url = `https://api.twitch.tv/helix/users?login=${username}`;
+export interface Clip {
+  id: string,
+  url: string,
+  embed_url: string,
+  broadcaster_id: string,
+  broadcaster_name: string,
+  creator_id: string,
+  creator_name: string,
+  video_id: string,
+  game_id: string,
+  language: string,
+  title: string,
+  view_count: number,
+  created_at: string,
+  thumbnail_url: string,
+  duration: number,
+  vod_offset: any
+}
+
+async function apiCall(url: string, token: string) {
   const res = await fetch(url, {
     headers: {
       Authorization: `Bearer ${token}`,
       'Client-Id': clientID
     }
   });
-  const json = await res.json();
-  return json.data[0];
+  return await res.json();
+}
+
+export async function getUser(username: string, token: string): Promise<User> {
+  const { data } = await apiCall(`https://api.twitch.tv/helix/users?login=${username}`, token);
+  return data[0];
+}
+
+export async function getAllClips(broadcasterID: string, token: string) {
+  const baseUrl = `https://api.twitch.tv/helix/clips?broadcaster_id=${broadcasterID}&first=100`;
+  let url = baseUrl;
+  const clips: Clip[] = [];
+  let cursor: string | undefined;
+
+  while (true) {
+    const result = await apiCall(url, token);
+    clips.push(...result.data);
+    cursor = result.pagination.cursor;
+    if (!cursor) {
+      break;
+    }
+    url = `${baseUrl}&after=${cursor}`;
+  }
+
+  return clips;
 }
